de2000755f67f5c0a9e3932fdc62bd63c3963eae
pjeutr <pjeutr@github.com>
2020-12-02 15:18:02 +0000

    forgot the app!

---
+++ www/lua/table.lua
@@ -0,0 +1,8 @@
+mytable = {}
+print("Type of mytable is ",type(mytable))
+
+mytable[1]= "Lua"
+mytable["wow"] = "Tutorial"
+
+alternatetable = mytable
+alternatetable["wow"] = "I changed it"
---
+++ www/timezone.php
@@ -0,0 +1,53 @@
+<?php 
+$title = "Timezone";
+$id = 4;
+
+include 'header.php';
+
+?>
+
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card strpied-tabled-with-hover">
+                    <div class="card-header ">
+                        <button id="twitter" class="btn btn-round"><i class="fa fa-user"></i>Add Timezone</button>
+                    </div>
+                    <div class="card-body table-full-width table-responsive">
+                        <table class="table table-hover table-striped">
+                            <thead>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>From</th>
+                                <th>Till</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>24 hours</td>
+                                    <td>00:00</td>
+                                    <td>00:00</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Working hours</td>
+                                    <td>06:00</td>
+                                    <td>19:00</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Evening</td>
+                                    <td>17:00</td>
+                                    <td>23:00</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>               
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php';?>
