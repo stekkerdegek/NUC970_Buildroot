73b04d06ad157822ff8d55ebb8dce125971188ed
pjeutr <pjeutr@github.com>
2020-12-10 09:32:40 +0000

    replace maasland_app with subtree

--- maasland_app/www/index.lua
+++
@@ -1,9 +0,0 @@
-output_contenttype = "text/html" 
-
-cwd = request["CWD"]
-
-output_include = { cwd .. "header.php", 
-                   cwd .. "groups.php",
-                   cwd .. "footer.php" }
-
-return CACHE_HIT
--- maasland_app/www/lua/table.lua
+++
@@ -1,8 +0,0 @@
-mytable = {}
-print("Type of mytable is ",type(mytable))
-
-mytable[1]= "Lua"
-mytable["wow"] = "Tutorial"
-
-alternatetable = mytable
-alternatetable["wow"] = "I changed it"
