0c52515c2e929d71b0ecc594aa5b5e9042ccbb5f
Peter <github@pjeutr.com>
2021-07-13 14:48:52 +0200

    uboot tools/rollback network

--- board/nuvoton/install_rootfs_match.sh
+++ board/nuvoton/install_rootfs_match.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
-APP_PATH=output/build/applications-1.0.0
 
- # cd board/nuvoton/rootfs-chili/usr/local/sbin/www/
- # make
- # cd /home/pjeutr/NUC970_Buildroot 
-
-#cd overlay/var/www/chili/
-#make -o cgi-bin
-#cd /home/pjeutr/NUC970_Buildroot
-
-#rm output/target/etc/resolv.conf
-#cp -af board/nuvoton/rootfs-chili/* output/target/
+#TOD replace cd with
+BOARD_DIR="$(dirname $0)"
+BOARD_NAME="$(basename ${BOARD_DIR})"
+# ${BOARD_DIR}/matcht2_test/drv/make
 
 #Make Wang test app
 cd board/nuvoton/match/match2_test/drv
@@ -35,8 +28,12 @@ cp -af board/nuvoton/match/match2_test/user/test_app overlay/scripts
 cp -af board/nuvoton/match/match2_test/user/uart4\&5/uart_test overlay/scripts
 cp -af board/nuvoton/match/wiegand-driver/wiegand-driver.ko overlay/scripts
 
-if [ -d $APP_PATH ]; then
-	cp $APP_PATH/yaffs2utils/mkyaffs2 output/target/usr/bin/
-	cp $APP_PATH/yaffs2utils/unyaffs2 output/target/usr/bin/
-	cp $APP_PATH/yaffs2utils/unspare2 output/target/usr/bin/
-fi
+#if [ -d $APP_PATH ]; then
+#	cp $APP_PATH/yaffs2utils/mkyaffs2 output/target/usr/bin/
+#	cp $APP_PATH/yaffs2utils/unyaffs2 output/target/usr/bin/
+#	cp $APP_PATH/yaffs2utils/unspare2 output/target/usr/bin/
+#fi
+
+#replace persistent config files by a symlink 
+#rm output/target/etc/network/interfaces
+#ln -s ../../maasland_app/etc/interfaces output/target/etc/network/interfaces
